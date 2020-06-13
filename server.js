3b7625685410788ab253ddff2daad3bfd24ea6d8
Himadri <himadris@gmail.com>
2020-06-13 11:42:14 -0700

    more stuff

--- server.js
+++ server.js
@@ -72,45 +72,26 @@ app.get("/add", function (req, res) {
   res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.get("/add", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
 // Displays all reservations
 app.get("/api/reservations", function (req, res) {
   return res.json(reservations);
 });
 
-// // Displays a single character, or returns false
-// app.get("/api/reservations/:character", function (req, res) {
-//   var chosen = req.params.character;
+var booked 
+if (reservations.length <= 5) {
+  booked = true;
+}
 
-//   console.log(chosen);
+else {
+  booked = false;
+}
+res.json (booked);
+})
 
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// // Create New Characters - takes in JSON input
-// app.post("/api/characters", function (req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   var newCharacter = req.body;
-
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newCharacter);
-
-//   characters.push(newCharacter);
-
-//   res.json(newCharacter);
-// });
-
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
+
